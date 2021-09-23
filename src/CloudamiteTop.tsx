fabf1ed7e92d93507397277c76a21861d2d8f088
Antti Pohjola <summeli@summeli.com>
2021-09-23 13:02:14 +0300

    new favicon, metadata, shareimg, and new look

--- src/CloudamiteTop.tsx
+++ src/CloudamiteTop.tsx
@@ -1,17 +1,13 @@
 import React  from 'react';
-import logoright from "./img/clouda-logo-right.png"; // Tell webpack this JS file uses this image
-import logoline from "./img/logo-line.png"; // Tell webpack this JS file uses this image
+import slogan from "./img/slogan.png"; // Tell webpack this JS file uses this image
 
 class CloudamiteTop extends React.Component {
 
     render() {
         return(
-            <div className="cloudamite-logo-top">
-                  <div className="logo-top-left">
-                    <img src={logoline} alt="Logol" />;
-                  </div>
-                  <div className="logo-top-right">
-                  <a href="https://cloudamite.com"><img src={logoright} alt="Logol"/></a>
+            <div className="cloudamite-slogan-top">
+                  <div className="cloudamite-slogan">
+                    <img src={slogan} alt="blow your mind" />
                   </div>
         </div>
       );
